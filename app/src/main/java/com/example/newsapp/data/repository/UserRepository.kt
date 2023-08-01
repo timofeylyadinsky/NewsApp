1b8e137cae55d7a4455c225341c157adce57b6bd
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-01 05:15:52 -0700

    feat: add save user method to repository

---
+++ app/src/main/java/com/example/newsapp/data/repository/UserRepository.kt
@@ -0,0 +1,18 @@
+package com.example.newsapp.data.repository
+
+
+import android.content.Context
+import androidx.room.Room
+import com.example.newsapp.data.db.UserDB
+import com.example.newsapp.data.entity.User
+
+
+fun saveUser(user: User, context: Context) {
+    val db = Room
+        .databaseBuilder(context, UserDB::class.java, "userInfo")
+        .build()
+    val userDao = db.userDao()
+    userDao.saveUser(user)
+}
+
+
