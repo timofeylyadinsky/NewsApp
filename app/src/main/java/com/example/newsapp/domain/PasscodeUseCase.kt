d524602ebec70047fc56342af0a3334746a61ddb
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-02 03:55:13 -0700

    feat: start domain layer for passcode part

---
+++ app/src/main/java/com/example/newsapp/domain/PasscodeUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.domain
+
+import com.example.newsapp.data.repository.UserRepository
+import javax.inject.Inject
+
+class PasscodeUseCase @Inject constructor(userRepository: UserRepository) {
+
+    fun checkStartApplication() {
+
+    }
+
+}
