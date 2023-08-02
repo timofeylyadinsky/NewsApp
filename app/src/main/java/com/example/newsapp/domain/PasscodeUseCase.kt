7eb8947f9be6ee0cab765a3ffec8a39cfb8d2f94
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-02 04:32:38 -0700

    feat: add first start function

--- app/src/main/java/com/example/newsapp/domain/PasscodeUseCase.kt
+++ app/src/main/java/com/example/newsapp/domain/PasscodeUseCase.kt
@@ -1,18 +1,20 @@
 package com.example.newsapp.domain
 
 import android.util.Log
+import com.example.newsapp.data.entity.User
 import com.example.newsapp.data.repository.UserRepository
 import javax.inject.Inject
 
 class PasscodeUseCase @Inject constructor(private val userRepository: UserRepository) {
 
-    suspend fun checkStartApplication() {
-        if(userRepository.getUserInfo().isEmpty())
-            Log.d("!!!!", "true")
-        else
-            Log.d("!!!!", "false")
+    suspend fun checkStartApplication() = userRepository.getUserInfo().isEmpty()
 
+
+    suspend fun firstStartPasscode(passcode: String) {
+        userRepository.saveUser(User(isLocked = true, passcode = passcode.toInt()))
     }
 
-
+    suspend fun firstStartPasscodeSkip() {
+        userRepository.saveUser(User(isLocked = false, passcode = 0))
+    }
 }
