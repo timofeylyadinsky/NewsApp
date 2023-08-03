064189537c397c480564cbb27c0c2be55d082a07
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-03 04:18:12 -0700

    feat: add passcode row

--- app/src/main/java/com/example/newsapp/ui/screen/PasscodeScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screen/PasscodeScreen.kt
@@ -1,2 +1,80 @@
 package com.example.newsapp.ui.screen
 
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+const val MAX_LENGTH = 4;
+
+@Composable
+@Preview(showBackground = true)
+fun PasscodeRow() {
+    var password by rememberSaveable { mutableStateOf("") }
+    val context = LocalContext.current
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        TextField(
+            value = password,
+            onValueChange = { if (it.length <= MAX_LENGTH) password = it },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+            modifier = Modifier
+                .padding(horizontal = 0.dp)
+                .fillMaxWidth(0.5f)
+        )
+        Button(
+            onClick = {
+                if (password.length < 4) {
+                    Toast.makeText(context, "Passcode should have 4 numbers", Toast.LENGTH_LONG)
+                        .show()
+                } else {
+                    /*TODO() Next Screen*/
+                }
+            },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Text(
+                text = "Submit",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
+}
