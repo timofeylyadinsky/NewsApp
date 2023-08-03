e33e99d255da3191c63823d1fc4604e39610a085
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-03 00:56:48 -0700

    feat: add first use case to class

--- app/src/main/java/com/example/newsapp/domain/PasscodeUseCases.kt
+++ app/src/main/java/com/example/newsapp/domain/PasscodeUseCases.kt
@@ -4,12 +4,10 @@ import com.example.newsapp.data.entity.User
 import com.example.newsapp.data.repository.UserRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class PasscodeUseCases @Inject constructor(private val userRepository: UserRepository) {
-
-    suspend fun isFirstStartApplication() = userRepository.getUserInfo() == null
-
 
     suspend fun savePasscodeFirstTime(passcode: String) {
         userRepository.saveUser(User(isLocked = true, passcode = passcode.toInt()))
@@ -24,14 +22,16 @@ class IsPasscodeRequiredUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
     ) {
-
+    suspend operator fun invoke() = withContext(defaultDispatcher) { userRepository.getUserInfo() == null }
 }
 
 class SavePasscodeUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
+    suspend operator fun invoke(passcode: String) = withContext(defaultDispatcher) {
 
+    }
 }
 
 class SkipPasscodeUseCase @Inject constructor(
