6ccf4764da450a9cce1ead474e3a6db6c3e72989
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-04 04:28:10 -0700

    fix: minor

--- app/src/main/java/com/example/newsapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/newsapp/data/api/ApiService.kt
@@ -1,19 +1,18 @@
 package com.example.newsapp.data.api
 
-import com.example.newsapp.R
 import com.example.newsapp.data.entity.News
 import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 val BASE_URL = "https://newsapi.org/"
 
 interface ApiService {
     @GET("v2/top-headlines")
-    fun getNews(@Path("apiKey") key: String, @Path("country") country: String ="us"): Call<News>
+    fun getNews(@Query("apiKey") key: String, @Query("country") country: String ="us"): Call<News>
 
     companion object Factory {
         fun create(): ApiService {
