9450a91fc0c7282b8d3884e70c5b3200c15ae575
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-04 04:15:05 -0700

    feat: add object to build api service

--- app/src/main/java/com/example/newsapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/newsapp/data/api/ApiService.kt
@@ -1,13 +1,29 @@
 package com.example.newsapp.data.api
 
-import com.example.newsapp.data.entity.Article
+import com.example.newsapp.R
+import com.example.newsapp.data.entity.News
+import okhttp3.OkHttpClient
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 val BASE_URL = "https://newsapi.org/"
 
 interface ApiService {
-    @GET("v2/top-headlines")
-    fun getArticle() : Call<Article>
+    @GET("v2/top-headlines?country=us&apiKey=")
+    fun getNews(): Call<News>
 
+    companion object Factory {
+        fun create(): ApiService {
+            val okHttpClient = OkHttpClient.Builder()
+                .build()
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
 }
