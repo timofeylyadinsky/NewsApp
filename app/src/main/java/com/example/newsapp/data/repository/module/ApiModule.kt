b4a770ad0acefa6740f6c7c099123ab2a013d9bb
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-04 05:06:12 -0700

    feat: add provides to base url

--- app/src/main/java/com/example/newsapp/data/repository/module/ApiModule.kt
+++ app/src/main/java/com/example/newsapp/data/repository/module/ApiModule.kt
@@ -1,11 +1,17 @@
 package com.example.newsapp.data.repository.module
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+
+const val BASE_URL = "https://newsapi.org/"
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
+
+    @Provides
+    fun providesBaseUrl(): String = BASE_URL
 
 }
