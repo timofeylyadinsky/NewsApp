cb67101faf20b93b7a7ae260cf2054215b079d54
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-04 03:10:26 -0700

    feat: add dependency retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,10 @@ dependencies {
     implementation(Deps.hiltNavigation)
     implementation(Deps.workKtx)
 
+    //Retrofit
+    implementation(Deps.retrofit)
+    implementation(Deps.retrofitGson)
+
     testImplementation(Deps.junit)
     androidTestImplementation(Deps.espressoCore)
     androidTestImplementation(platform(Deps.composeBom))
---
+++ app/src/main/java/com/example/newsapp/data/api/ApiService.kt
@@ -0,0 +1,4 @@
+package com.example.newsapp.data.api
+
+class ApiService {
+}
