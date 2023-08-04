5877ecbf7d6faf1c6ca272d23d339cc4f83934eb
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-04 03:37:15 -0700

    feat: start api service interface

--- app/src/main/java/com/example/newsapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/newsapp/data/api/ApiService.kt
@@ -1,4 +1,13 @@
 package com.example.newsapp.data.api
 
-class ApiService {
+import com.example.newsapp.data.entity.Article
+import retrofit2.Call
+import retrofit2.http.GET
+
+val BASE_URL = "https://newsapi.org/"
+
+interface ApiService {
+    @GET("v2/top-headlines")
+    fun getArticle() : Call<Article>
+
 }
