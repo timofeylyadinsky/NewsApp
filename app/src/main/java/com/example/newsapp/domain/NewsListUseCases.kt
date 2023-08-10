e1dff058f3baddfd1fcb7d9e73e798473dd5fd92
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-10 02:03:07 -0700

    feat: add mapping usecase

--- app/src/main/java/com/example/newsapp/domain/NewsListUseCases.kt
+++ app/src/main/java/com/example/newsapp/domain/NewsListUseCases.kt
@@ -4,6 +4,8 @@ import com.example.newsapp.data.api.NetworkResult
 import com.example.newsapp.data.entity.NewsDto
 import com.example.newsapp.data.repository.NewsRepository
 import com.example.newsapp.data.repository.module.IoDispatcher
+import com.example.newsapp.domain.entity.News
+import com.example.newsapp.domain.entity.toNews
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -17,9 +19,17 @@ class GetNewsResponseUseCase @Inject constructor(
     }
 }
 
-class GetArticleListUseCase @Inject constructor(
-    private val newsRepository: NewsRepository,
+class MapArticleListUseCase @Inject constructor(
+    private val getNewsResponseUseCase: GetNewsResponseUseCase,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
-
+    suspend operator fun invoke() = withContext(ioDispatcher) {
+        when (val response = getNewsResponseUseCase.invoke()) {
+            is NetworkResult.Success -> NetworkResult.Success(response.data.toNews())
+            is NetworkResult.Error -> NetworkResult.Error(
+                code = response.code,
+                message = response.message
+            )
+        }
+    }
 }
