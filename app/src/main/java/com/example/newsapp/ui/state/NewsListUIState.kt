c2ac9023f098c84c8f0e8ead4acb6ac9f3f31ad1
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-11 03:26:18 -0700

    feat: add news list ui state

---
+++ app/src/main/java/com/example/newsapp/ui/state/NewsListUIState.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.ui.state
+
+import com.example.newsapp.domain.entity.Article
+
+sealed class NewsListUIState {
+    object LOADING: NewsListUIState()
+    data class SUCCESS(val articles: List<Article>) : NewsListUIState()
+    data class ERROR(val message: String) : NewsListUIState()
+}
