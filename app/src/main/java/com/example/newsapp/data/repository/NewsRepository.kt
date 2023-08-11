016b4da202c1056381e71e677f145ba171e6a96c
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-11 02:47:36 -0700

    feat: change network util impl

--- app/src/main/java/com/example/newsapp/data/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/data/repository/NewsRepository.kt
@@ -3,13 +3,12 @@ package com.example.newsapp.data.repository
 import com.example.newsapp.data.api.ApiService
 import com.example.newsapp.data.api.NetworkResult
 import com.example.newsapp.data.entity.NewsDto
-import com.example.newsapp.data.repository.util.NetworkUtil
-import retrofit2.Response
+import com.example.newsapp.data.repository.util.handleApi
 import javax.inject.Inject
 
 class NewsRepository @Inject constructor(
     private val apiService: ApiService
 ) {
     suspend fun dataSource(): NetworkResult<NewsDto> =
-        NetworkUtil().handleApi { apiService.getNews() }
+        handleApi { apiService.getNews() }
 }
--- app/src/main/java/com/example/newsapp/data/repository/util/NetworkUtil.kt
+++ app/src/main/java/com/example/newsapp/data/repository/util/NetworkUtil.kt
@@ -3,20 +3,18 @@ package com.example.newsapp.data.repository.util
 import com.example.newsapp.data.api.NetworkResult
 import retrofit2.Response
 
-class NetworkUtil {
-    suspend fun <T : Any> handleApi(
-        execute: suspend () -> Response<T>
-    ): NetworkResult<T> {
-        return try {
-            val response = execute()
-            val body = response.body()
-            if (response.isSuccessful && body != null) {
-                NetworkResult.Success(body)
-            } else {
-                NetworkResult.Error(response.code(), response.message())
-            }
-        } catch (e: Exception) {
-            NetworkResult.Error(e.hashCode(), e.message)
+suspend fun <T : Any> handleApi(
+    execute: suspend () -> Response<T>
+): NetworkResult<T> {
+    return try {
+        val response = execute()
+        val body = response.body()
+        if (response.isSuccessful && body != null) {
+            NetworkResult.Success(body)
+        } else {
+            NetworkResult.Error(response.code(), response.message())
         }
+    } catch (e: Exception) {
+        NetworkResult.Error(e.hashCode(), e.message)
     }
 }
