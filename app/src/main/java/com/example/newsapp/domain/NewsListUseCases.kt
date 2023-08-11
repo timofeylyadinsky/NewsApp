b30cc5eef926542db6a0f9b8b131afee8f68fdd5
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-11 02:50:07 -0700

    fix: news list use case impl

--- app/src/main/java/com/example/newsapp/domain/NewsListUseCases.kt
+++ app/src/main/java/com/example/newsapp/domain/NewsListUseCases.kt
@@ -17,25 +17,10 @@ class GetNewsResponseUseCase @Inject constructor(
     private val newsRepository: NewsRepository,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
-    suspend operator fun invoke(): NetworkResult<News> = withContext(ioDispatcher) {
-        when (val response = newsRepository.dataSource()) {
-            is NetworkResult.Success -> NetworkResult.Success(data = response.data.toNews())
-            is NetworkResult.Error -> NetworkResult.Error(
-                code = response.code,
-                message = response.message
-            )
-        }
-    }
-}
-
-class MapArticleListUseCase @Inject constructor(
-    private val getNewsResponseUseCase: GetNewsResponseUseCase,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
-) {
     suspend operator fun invoke(): Flow<NewsData> = withContext(ioDispatcher) {
         flow {
-            when (val response = getNewsResponseUseCase.invoke()) {
-                is NetworkResult.Success -> emit(NewsData(response.data.articles))
+            when (val response = newsRepository.dataSource()) {
+                is NetworkResult.Success -> emit(NewsData(response.data.toNews().articles))
                 is NetworkResult.Error -> emit(NewsData(errorMessage = "${response.code} : ${response.message}"))
             }
         }
