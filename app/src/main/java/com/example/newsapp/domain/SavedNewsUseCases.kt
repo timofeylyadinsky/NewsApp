46b3b4a766d69da4e347272d6445f0c22b166be4
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-15 05:32:43 -0700

    feat: add save news use case

---
+++ app/src/main/java/com/example/newsapp/domain/SavedNewsUseCases.kt
@@ -0,0 +1,17 @@
+package com.example.newsapp.domain
+
+import com.example.newsapp.data.entity.SavedNewsDbo
+import com.example.newsapp.data.repository.NewsRepository
+import com.example.newsapp.data.repository.module.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SaveNewsUseCase @Inject constructor(
+    private val newsRepository: NewsRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) {
+    suspend fun invoke(url: String) = withContext(ioDispatcher) {
+        newsRepository.saveNews(SavedNewsDbo(url = url))
+    }
+}
