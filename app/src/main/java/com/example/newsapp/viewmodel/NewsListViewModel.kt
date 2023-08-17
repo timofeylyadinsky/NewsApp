eca3ab4264e413fafc830a9ee52f5772fafb55ea
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-17 02:51:31 -0700

    feat: add getting list from db in vm

--- app/src/main/java/com/example/newsapp/viewmodel/NewsListViewModel.kt
+++ app/src/main/java/com/example/newsapp/viewmodel/NewsListViewModel.kt
@@ -2,7 +2,10 @@ package com.example.newsapp.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.newsapp.domain.DeleteNewsInDBUseCase
+import com.example.newsapp.domain.GetNewsFromDBUseCase
 import com.example.newsapp.domain.GetNewsResponseUseCase
+import com.example.newsapp.domain.InsertCachedArticlesUseCase
 import com.example.newsapp.ui.state.NewsListUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,27 +14,40 @@ import javax.inject.Inject
 
 @HiltViewModel
 class NewsListViewModel @Inject constructor(
-    private val getNewsResponseUseCase: GetNewsResponseUseCase
+    private val getNewsResponseUseCase: GetNewsResponseUseCase,
+    private val getNewsFromDBUseCase: GetNewsFromDBUseCase,
+    private val insertCachedArticlesUseCase: InsertCachedArticlesUseCase,
+    private val deleteNewsInDBUseCase: DeleteNewsInDBUseCase
 ) : ViewModel() {
 
     val uiState = MutableStateFlow<NewsListUIState>(NewsListUIState.LOADING)
 
     init {
+        uiState.tryEmit(NewsListUIState.LOADING)
         getList()
     }
 
+//    private fun getList() = viewModelScope.launch {
+//        uiState.tryEmit(NewsListUIState.LOADING)
+//        try {
+//            val newsData = getNewsResponseUseCase()
+//            newsData.collect { data ->
+//                if (data.errorMessage.isNullOrEmpty()) uiState.value =
+//                    NewsListUIState.SUCCESS(data.news)
+//                else uiState.value =
+//                    NewsListUIState.ERROR(data.errorMessage)
+//            }
+//        } catch (e: Exception) {
+//            uiState.value = NewsListUIState.ERROR(e.localizedMessage)
+//        }
+//    }
+
     private fun getList() = viewModelScope.launch {
-        uiState.tryEmit(NewsListUIState.LOADING)
-        try {
-            val newsData = getNewsResponseUseCase()
-            newsData.collect { data ->
-                if (data.errorMessage.isNullOrEmpty()) uiState.value =
-                    NewsListUIState.SUCCESS(data.news)
-                else uiState.value =
-                    NewsListUIState.ERROR(data.errorMessage)
-            }
-        } catch (e: Exception) {
-            uiState.value = NewsListUIState.ERROR(e.localizedMessage)
+        getNewsFromDBUseCase().collect {
+            uiState.tryEmit(NewsListUIState.LOADING)
+            uiState.value = NewsListUIState.SUCCESS(it)
         }
     }
+
+    private fun fetchList() = viewModelScope.launch {  }
 }
