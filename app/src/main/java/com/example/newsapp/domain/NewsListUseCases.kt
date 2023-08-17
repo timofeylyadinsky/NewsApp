95802edbe8fb2ff0a8836c0364d9b2874d4942da
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-17 02:32:38 -0700

    feat: add get from db use case

--- app/src/main/java/com/example/newsapp/domain/NewsListUseCases.kt
+++ app/src/main/java/com/example/newsapp/domain/NewsListUseCases.kt
@@ -3,11 +3,14 @@ package com.example.newsapp.domain
 import com.example.newsapp.data.api.NetworkResult
 import com.example.newsapp.data.repository.NewsRepository
 import com.example.newsapp.data.repository.module.IoDispatcher
+import com.example.newsapp.domain.entity.Article
 import com.example.newsapp.domain.entity.NewsData
+import com.example.newsapp.domain.entity.toArticle
 import com.example.newsapp.domain.entity.toNews
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -20,6 +23,19 @@ class GetNewsResponseUseCase @Inject constructor(
             when (val response = newsRepository.dataSource()) {
                 is NetworkResult.Success -> emit(NewsData(response.data.toNews().articles))
                 is NetworkResult.Error -> emit(NewsData(errorMessage = "${response.code} : ${response.message}"))
+            }
+        }
+    }
+}
+
+class GetNewsFromDBUseCase @Inject constructor(
+    private val newsRepository: NewsRepository,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) {
+    suspend operator fun invoke(): Flow<List<Article>> = withContext(ioDispatcher) {
+        newsRepository.getCachedArticles().map {
+            it.map { item ->
+                item.toArticle()
             }
         }
     }
