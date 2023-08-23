4282db0a626a60393f8382f4757c3815dfbc8991
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-23 05:54:56 -0700

    feat: add two tests

--- app/src/androidTest/java/com/example/newsapp/integration/UserRepositoryIntegrationTest.kt
+++ app/src/androidTest/java/com/example/newsapp/integration/UserRepositoryIntegrationTest.kt
@@ -1,32 +1,29 @@
 package com.example.newsapp.integration
 
 import android.content.Context
-import android.util.Log
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.example.newsapp.data.dao.UserDao
 import com.example.newsapp.data.db.UserDB
 import com.example.newsapp.data.entity.User
-import com.example.newsapp.data.repository.module.DatabaseModule
+import com.example.newsapp.data.repository.UserRepository
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-import javax.inject.Inject
-import javax.inject.Named
 
 @HiltAndroidTest
 class UserRepositoryIntegrationTest {
     @get:Rule(order = 0)
     var hiltRule = HiltAndroidRule(this)
 
-    lateinit var db: UserDB
+    private lateinit var db: UserDB
     private lateinit var userDao: UserDao
+    private lateinit var userRepository: UserRepository
 
     @Before
     fun setup() {
@@ -34,13 +31,28 @@ class UserRepositoryIntegrationTest {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(context, UserDB::class.java).build()
         userDao = db.userDao()
-        //userDao.saveUser(User(isLocked = true, passcode = "1111"))
+        userRepository = UserRepository(userDao)
     }
 
     @Test
-    fun daoTest() {
-        Log.d("!!!!!!!!", userDao.getUser().toString())
+    fun whenDBEmptyThenGetEmptyUserTest() {
+        runTest {
+            assertThat(userRepository.getUserInfo()).isNull()
+        }
     }
+
+    @Test
+    fun givenUserWhenDBGetInfoThenGetThatUser() {
+        runTest {
+            val expectedUser = User(isLocked = true, passcode = "1111")
+            userDao.saveUser(expectedUser)
+
+            val actualUser = userRepository.getUserInfo()
+
+            assertThat(actualUser).isEqualTo(expectedUser)
+        }
+    }
+
 
     @After
     fun tearDown() {
