af8d72ee77909036a927373c4eed3d23ff1bc29c
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-23 05:40:12 -0700

    feat: change impl of test db

--- app/src/androidTest/java/com/example/newsapp/di/TestAppModule.kt
+++ app/src/androidTest/java/com/example/newsapp/di/TestAppModule.kt
@@ -1,25 +1,24 @@
 package com.example.newsapp.di
 
 import android.app.Application
-import android.content.Context
 import androidx.room.Room
 import com.example.newsapp.data.db.UserDB
+import com.example.newsapp.data.repository.module.DatabaseModule
 import dagger.Module
 import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-class TestAppModule {
-    //    @Provides
-//    fun provideUserDao(userDB: UserDB) = userDB.userDao()
-//
-    @Provides
-    @Singleton
-    fun provideInMemoryUserDB(app: Application) = Room
-        .inMemoryDatabaseBuilder(app, UserDB::class.java)
-        .build()
-}
+//@Module
+//@TestInstallIn(components = [SingletonComponent::class], replaces = [DatabaseModule::class])
+//class TestAppModule {
+//    //    @Provides
+////    fun provideUserDao(userDB: UserDB) = userDB.userDao()
+////
+//    @Provides
+//    @Singleton
+//    fun provideInMemoryUserDB(app: Application) = Room
+//        .inMemoryDatabaseBuilder(app, UserDB::class.java)
+//        .build()
+//}
--- app/src/androidTest/java/com/example/newsapp/integration/UserRepositoryIntegrationTest.kt
+++ app/src/androidTest/java/com/example/newsapp/integration/UserRepositoryIntegrationTest.kt
@@ -1,7 +1,12 @@
 package com.example.newsapp.integration
 
+import android.content.Context
+import android.util.Log
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import com.example.newsapp.data.dao.UserDao
 import com.example.newsapp.data.db.UserDB
+import com.example.newsapp.data.entity.User
 import com.example.newsapp.data.repository.module.DatabaseModule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -16,25 +21,25 @@ import javax.inject.Inject
 import javax.inject.Named
 
 @HiltAndroidTest
-@UninstallModules(DatabaseModule::class)
 class UserRepositoryIntegrationTest {
     @get:Rule(order = 0)
     var hiltRule = HiltAndroidRule(this)
 
-    @Inject
-    @Named("test_db")
     lateinit var db: UserDB
     private lateinit var userDao: UserDao
 
     @Before
     fun setup() {
         hiltRule.inject()
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, UserDB::class.java).build()
         userDao = db.userDao()
+        //userDao.saveUser(User(isLocked = true, passcode = "1111"))
     }
 
     @Test
     fun daoTest() {
-        print(userDao.getUser())
+        Log.d("!!!!!!!!", userDao.getUser().toString())
     }
 
     @After
