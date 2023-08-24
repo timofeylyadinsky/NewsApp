5b16a7b1b2d13c3e23cbe013d32a7fa83142647c
Timofey Lyadinsky <tima.led12@gmail.com>
2023-08-24 05:22:47 -0700

    feat: add news use cases integration tests

--- app/src/androidTest/java/com/example/newsapp/integration/NewsUseCasesIntegrationTest.kt
+++ app/src/androidTest/java/com/example/newsapp/integration/NewsUseCasesIntegrationTest.kt
@@ -9,11 +9,21 @@ import com.example.newsapp.data.dao.SavedNewsDao
 import com.example.newsapp.data.db.NewsDB
 import com.example.newsapp.data.db.SavedNewsDB
 import com.example.newsapp.data.repository.NewsRepository
+import com.example.newsapp.domain.DeleteNewsInDBUseCase
+import com.example.newsapp.domain.GetNewsFromDBUseCase
+import com.example.newsapp.domain.InsertCachedArticlesUseCase
+import com.example.newsapp.integration.util.ArticlesLists
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -28,6 +38,12 @@ class NewsUseCasesIntegrationTest {
     private lateinit var savedNewsDao: SavedNewsDao
     private lateinit var apiService: ApiService
     private lateinit var newsRepository: NewsRepository
+
+    private lateinit var getNewsFromDBUseCase: GetNewsFromDBUseCase
+    private lateinit var deleteNewsInDBUseCase: DeleteNewsInDBUseCase
+    private lateinit var insertCachedArticlesUseCase: InsertCachedArticlesUseCase
+
+    private var ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 
     @Before
     fun setup() {
@@ -49,6 +65,53 @@ class NewsUseCasesIntegrationTest {
         )
     }
 
+    @Test
+    fun givenListInDBWhenGetItThenReturnMappedIt() {
+        runTest {
+            getNewsFromDBUseCase = GetNewsFromDBUseCase(newsRepository, ioDispatcher)
+            newsDao.insertAllArticles(ArticlesLists.expectedArticleDboList)
+
+            val actualArticles = getNewsFromDBUseCase().first()
+
+            assertThat(actualArticles).isEqualTo(ArticlesLists.expectedArticleList)
+
+        }
+    }
+
+    @Test
+    fun givenEmptyDBWhenGetItThenReturnEmptyList() {
+        runTest {
+            getNewsFromDBUseCase = GetNewsFromDBUseCase(newsRepository, ioDispatcher)
+
+            val actualArticles = getNewsFromDBUseCase().first()
+
+            assertThat(actualArticles).isEmpty()
+
+        }
+    }
+
+    @Test
+    fun givenArticlesListWhenInsertItThenInDBIt() {
+        runTest {
+            insertCachedArticlesUseCase = InsertCachedArticlesUseCase(newsRepository, ioDispatcher)
+
+            insertCachedArticlesUseCase(ArticlesLists.expectedArticleList)
+
+            val actualArticles = newsDao.getAllCachedArticles().first()
+
+            assertThat(actualArticles).isEqualTo(ArticlesLists.expectedArticleDboList)
+        }
+    }
+
+    @Test
+    fun givenArticlesInDBWhenDeleteItThenDBISEmpty() {
+        runTest {
+            deleteNewsInDBUseCase = DeleteNewsInDBUseCase(newsRepository, ioDispatcher)
+            deleteNewsInDBUseCase()
+            val actualArticles = newsDao.getAllCachedArticles().first()
+            assertThat(actualArticles).isEmpty()
+        }
+    }
 
     @After
     fun tearDown() {
